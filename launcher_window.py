b5f2af8bbba4f02bf415a881e672d654d9a3a8f4
janakhpon <jnovaxer@gmail.com>
2019-07-30 21:28:24 +0630

    GDEV002

--- launcher_window.py
+++ launcher_window.py
@@ -8,14 +8,14 @@ class Commander:
         self.cancel = ["nope", "no", "noo", "not yet", "don't", "do not", "stop", "wait", "hold on", "not now"]
 
     def discover(self, text):
-        if "launch code" or "open code" in text:
+        if "launch" or "open" in text:
             app = text.split(" ", 1)[-1]  # expression in python 1 equals the second word
             self.respond("Opening " + app)
             if app == "notepad":
-                
-                pdf = "path/to/pdf"
-                acrobatPath = r'C:\Program Files\Adobe\Reader 9.0\Reader\AcroRd32.exe'
-                subprocess.Popen("%s %s" % (acrobatPath, pdf))
+                acrobatPath = r'C:\Windows\system32\notepad.exe'
+                # subprocess.run(['notepad.exe', 'test.txt'])
+                os.system('"C://Temp/a b c/Notepad.exe"')
+
 
     def respond(self, response):
         print(response)
